add06d5ff204fcaedf24cd2070595cac90b1436f
suzukey <suzukei0208@gmail.com>
2020-09-06 14:25:17 +0900

    ♻️ Rewrite to readable code in several places

--- majocol/color.py
+++ majocol/color.py
@@ -1,29 +1,26 @@
+import numpy as np
 from sklearn.cluster import KMeans
 
-from majocol.utils import is_positive_int
+from majocol.utils import is_positive_int, rgb2hex
 from majocol.types import HexRgbList
 
 
-def pick(rgb_image, count: int = 1) -> HexRgbList:
+def pick(image_ndarray, count: int = 1) -> HexRgbList:
     """
     Pick major colors from image
     """
     if not is_positive_int(count):
         return None
 
-    try:
-        image_shape = rgb_image.shape[0] * rgb_image.shape[1]
-        img = rgb_image.reshape(image_shape, 3)
-    except BaseException:
-        return None
+    image_height = image_ndarray.shape[0]
+    image_width = image_ndarray.shape[1]
+
+    image_size = image_height * image_width
+    rgb_ndarray = image_ndarray.reshape(image_size, 3)
 
     cluster = KMeans(n_clusters=count)
-    cluster.fit(X=img)
+    cluster.fit(X=rgb_ndarray)
+    centers_arr = cluster.cluster_centers_.astype(np.uint8, copy=False)
 
-    centers_arr = cluster.cluster_centers_.astype(int, copy=False)
-    colors_hex = []
-    for rgb_arr in centers_arr:
-        color_hex = '%02x%02x%02x' % tuple(rgb_arr)
-        colors_hex.append(color_hex)
-
-    return colors_hex
+    hex_colors = [rgb2hex(*dec_colors) for dec_colors in centers_arr]
+    return hex_colors
--- majocol/utils.py
+++ majocol/utils.py
@@ -1,3 +1,4 @@
+from functools import lru_cache
 from typing import Any
 
 
@@ -6,3 +7,11 @@ def is_positive_int(target: Any) -> bool:
     Whether `target` is positive integer or not
     """
     return isinstance(target, int) and target > 0
+
+
+@lru_cache(maxsize=None)
+def rgb2hex(r, g, b):
+    """
+    Convert RGB color from decimal to hexadecimal
+    """
+    return '#%02x%02x%02x' % (r, g, b)
