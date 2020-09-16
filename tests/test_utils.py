7156af67181886c66832fd8afb976996151a29cb
suzukey <suzukei0208@gmail.com>
2020-09-16 16:02:10 +0900

    ✅ Append type hint to the test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -9,21 +9,21 @@ from majocol.utils import is_positive_int, rgb2hex
         (1, True),
         (100, True),
         (0, False),
-        ('1', False),
+        ("1", False),
         (3.14, False),
     ],
 )
-def test_is_positive_int(target, expected_response):
+def test_is_positive_int(target: int, expected_response: bool) -> None:
     assert is_positive_int(target) is expected_response
 
 
 @pytest.mark.parametrize(
     "r,g,b,expected_response",
     [
-        (255, 255, 255, '#ffffff'),
-        (0, 0, 0, '#000000'),
-        (29, 80, 162, '#1d50a2'),
+        (255, 255, 255, "#ffffff"),
+        (0, 0, 0, "#000000"),
+        (29, 80, 162, "#1d50a2"),
     ],
 )
-def test_rgb2hex(r, g, b, expected_response):
+def test_rgb2hex(r: int, g: int, b: int, expected_response: str) -> None:
     assert rgb2hex(r, g, b) == expected_response
