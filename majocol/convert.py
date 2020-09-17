9b7dae7b238f5fbcabcbdb8be52d433f352eff36
suzukey <suzukei0208@gmail.com>
2020-09-17 20:14:33 +0900

    🔥 Does not support opencv image conversion

--- majocol/convert.py
+++ majocol/convert.py
@@ -22,16 +22,3 @@ def pillow_to_rgb_ndarr(image_pillow: Image) -> np.ndarray:
     image_ndarray = np.array(image_pillow.convert("RGB"))
 
     return image_ndarray
-
-
-def cv2_to_rgb_ndarr(image_cv2: np.ndarray) -> np.ndarray:
-    """
-    Convert color order from BGR to RGB
-
-    Requirements: `cv2(opencv-python)`
-    """
-    import cv2
-
-    image_ndarray = cv2.cvtColor(image_cv2, cv2.COLOR_BGR2RGB)
-
-    return image_ndarray
